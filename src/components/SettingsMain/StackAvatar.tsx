f0c872559fc84d6bab9b8821e4672c8509a36ffb
Ken Osagie <kenosagie88@gmail.com>
2023-01-04 16:44:47 +0100

    created stack avatar

---
+++ src/components/SettingsMain/StackAvatar.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import Image from "next/image";
+type Props = {
+  users: number;
+};
+
+export default function StackAvatar({ users }: Props) {
+  const [srcs, setSrcs] = React.useState([
+    "/avatar.png",
+    "/avatar_1.png",
+    "/avatar_2.png",
+    "/avatar_3.png",
+    "/avatar_4.png",
+  ]);
+  useEffect(() => {
+    setSrcs(srcs.slice(0, users));
+  }, []);
+
+  return (
+    <div className="flex -space-x-2">
+      {users > 5 ? (
+        <>
+          {srcs.map((each, i) => {
+            return (
+              <Image
+                className="w-7 h-7 rounded-full "
+                src={each}
+                alt=""
+                width={40}
+                height={40}
+                key={i}
+              />
+            );
+          })}
+          <a
+            className="flex justify-center items-center w-7 h-7 text-xs font-medium text-brand-card-6 bg-brand-background-1 rounded-full"
+            href="#"
+          >
+            +{users - 5}
+          </a>
+        </>
+      ) : (
+        srcs.map((each, index, arr) => {
+          return (
+            <Image
+              className="w-7 h-7 rounded-full "
+              src={each}
+              alt=""
+              width={40}
+              height={40}
+              key={index}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+}
